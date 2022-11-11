b30c53553ce413baf0ec138840db51f4bd8b37f4
zazaroyana123 <muhammadjajaroyana3@gmail.com>
2022-11-11 16:06:51 +0700

    membuat admin

---
+++ application/views/admin/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Web Prog II | Merancang Template sederhana dengan codeigniter </title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/CSS/pelayanan.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <hgroup>
+                <h1> SIPEDES</h1>
+                <h3> Sistem Pelayanan Desa Paradise </h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo base_url() ?>admin">Dhasboard </a></li>
+                    <li><a href="<?php echo base_url() ?>admin/pengajuan">Pengajuan</a></li>
+                    <li><a href="<?php echo base_url() ?>admin/pengaduan">Pengaduan </a></li>
+                    <li><a href="<?php echo base_url() ?>admin/profile">Profile </a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </header>
